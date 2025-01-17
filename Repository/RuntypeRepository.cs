ea4b2ed78963e0981adcb96b7ec723dc801ec9b4
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:30:15 +0100

    Create RunTypeRepository

---
+++ Repository/RuntypeRepository.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using RunTracker.Model;
+
+namespace RunTracker.Repository
+{
+    internal class RunTypeRepository : IRunTypeRepository
+    {
+        private readonly IMongoCollection<RunType> _runTypesCollection;
+
+        public RunTypeRepository(IMongoDatabase database)
+        {
+            _runTypesCollection = database.GetCollection<RunType>("RunTypes");
+        }
+
+        public async Task<List<RunType>> GetAllAsync()
+        {
+            return await _runTypesCollection.Find(r => true).ToListAsync();
+        }
+
+        public async Task AddAsync(RunType runType)
+        {
+            await _runTypesCollection.InsertOneAsync(runType);
+        }
+
+        public async Task UpdateAsync(RunType runType)
+        {
+            var filter = Builders<RunType>.Filter.Eq(r => r.Id, runType.Id);
+            await _runTypesCollection.ReplaceOneAsync(filter, runType);
+        }
+
+        public async Task DeleteAsync(ObjectId id)
+        {
+            var filter = Builders<RunType>.Filter.Eq(r => r.Id, id);
+            await _runTypesCollection.DeleteOneAsync(filter);
+        }
+    }
+}
