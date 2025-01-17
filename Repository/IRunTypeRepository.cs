85cdec59ae37ac37c563cd54ac810fa8de021aa4
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:29:43 +0100

    Create IRunTypeRepository interface

---
+++ Repository/IRunTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using RunTracker.Model;
+
+namespace RunTracker.Repository
+{
+    public interface IRunTypeRepository
+    {
+        Task<List<RunType>> GetAllAsync();
+        Task AddAsync(RunType runType);
+        Task UpdateAsync(RunType runType);
+        Task DeleteAsync(ObjectId id);
+    }
+}
