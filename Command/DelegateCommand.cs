3b3ba3639730e566c0e851331137d736b7e20ee6
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:31:09 +0100

    Create DelegateCommand class

---
+++ Command/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RunTracker.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object?, bool> canExecute;
+        private DelegateCommand? openPackOption;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute, Func<object?, bool> canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public DelegateCommand(DelegateCommand? openPackOption)
+        {
+            this.openPackOption = openPackOption;
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object? parameter) => canExecute is null ? true : canExecute(parameter);
+
+        public void Execute(object? parameter) => execute(parameter);
+    }
+}
