3aa45fe889c419fa1e0184efb19462bfbc4167e7
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:29:21 +0100

    Create IRunningSessionRepository interface

---
+++ Repository/IRunningSessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using RunTracker.Model;
+
+namespace RunTracker.Repository
+{
+    public interface IRunningSessionRepository
+    {
+        Task<List<RunningSession>> GetAllAsync();
+        Task AddAsync(RunningSession runningSession);
+        Task UpdateAsync(RunningSession runningSession);
+        Task DeleteAsync(ObjectId id);
+    }
+}
