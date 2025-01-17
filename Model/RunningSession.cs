1cacfb6356aa3e22dc51568444fc203f6a6bdbf4
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:28:23 +0100

    Create RunningSession model class

---
+++ Model/RunningSession.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace RunTracker.Model
+{
+    public class RunningSession
+    {
+        public ObjectId Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Distance { get; set; }
+        public TimeSpan Time { get; set; }
+        public string RunType { get; set; }
+    }
+}
