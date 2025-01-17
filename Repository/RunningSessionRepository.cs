f7f52318f5c7803d2b5aaee9bf7828533c562c5c
sofia <sofia.k.nordstrom@gmail.com>
2025-01-17 12:30:00 +0100

    Create RunningSessionRepository

---
+++ Repository/RunningSessionRepository.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using RunTracker.Model;
+
+namespace RunTracker.Repository
+{
+    public class RunningSessionRepository : IRunningSessionRepository
+    {
+
+        private readonly IMongoCollection<RunningSession> _runningSessionsCollection;
+
+        public RunningSessionRepository(IMongoDatabase database)
+        {
+            _runningSessionsCollection = database.GetCollection<RunningSession>("RunningSessions");
+        }
+
+        public async Task<List<RunningSession>> GetAllAsync()
+        {
+            return await _runningSessionsCollection.Find(r => true).ToListAsync();
+        }
+
+        public async Task AddAsync(RunningSession runningSession)
+        {
+            await _runningSessionsCollection.InsertOneAsync(runningSession);
+        }
+
+        public async Task UpdateAsync(RunningSession runningSession)
+        {
+            var filter = Builders<RunningSession>.Filter.Eq(r => r.Id, runningSession.Id);
+            await _runningSessionsCollection.ReplaceOneAsync(filter, runningSession);
+        }
+
+        public async Task DeleteAsync(ObjectId id)
+        {
+            var filter = Builders<RunningSession>.Filter.Eq(r => r.Id, id);
+            await _runningSessionsCollection.DeleteOneAsync(filter);
+        }
+    }
+}
